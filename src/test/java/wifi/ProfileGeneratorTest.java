ab3ec08b8acec97b3e97bd98b3d7ce76ffc2bce8
彭鑫19536 <pengxin19536@hundsun.com>
2019-04-26 11:50:35 +0800

    破解WiFi工具

--- src/test/java/wifi/ProfileGeneratorTest.java
+++ src/test/java/wifi/ProfileGeneratorTest.java
@@ -2,19 +2,6 @@ package wifi;
 
 import org.testng.annotations.Test;
 
-/**
- * 文件描述
- * ProductName :    Hundsun UF30
- * ProjectName :    适当性
- * Package :        wifi
- * Description :
- *
- * @author :        pengxin19536
- * @version :       1.0
- * UpdateRemark :
- * Copyright © 2019 Hundsun Technologies Inc. All Rights Reserved
- * @date :          2019/4/26
- */
 public class ProfileGeneratorTest {
 
     @Test
